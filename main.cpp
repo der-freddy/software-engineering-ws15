80898573f13b40be07c355198e8bd0c689800d00
Freddy <frederik.sukop@uni-weimar.de>
2015-11-04 11:38:19 +0100

    finales added

--- main.cpp
+++ main.cpp
@@ -7,42 +7,58 @@
 
 #include <memory>
 #include <iostream>
-
+#include <string>
 int main(int argc, char* argv[])
 {
+
+  std::string arg1 = argv[1];
+  double arg2 = std::stod(argv[2]);
+
+
+  if(arg1 == "DollarToEuro")
+  {
   // Dollar to Euro Converter
-  auto myConverter = std::make_shared<DollarToEuroConverter>();
-  double aLotOfDollars = 10000;
-  double aLotOfEuros = myConverter->convert(aLotOfDollars);
-  std::cout << myConverter->toString() << " has converted "<< aLotOfDollars << " Dollar to " << aLotOfEuros <<" Euros!\n"<<std::endl;
+    auto myConverter = std::make_shared<DollarToEuroConverter>();
+    double aLotOfEuros = myConverter->convert(arg2);
+    std::cout << myConverter->toString() << " has converted "<< arg2 << " Dollar to " << aLotOfEuros <<" Euros!\n"<<std::endl;
+  }
 
+  else if(arg1 == "YenToEuro")
+  {
   // Yen to Euro Converter
+    auto yentoeuro = std::make_shared<YenToEuroConverter>();
+    double aLotOfEuros = yentoeuro->convert(arg2);
+    std::cout << yentoeuro->toString() << " has converted "<< arg2 << " Yen to " << aLotOfEuros <<" Euros!\n"<<std::endl;
+  }
 
-  auto yentoeuro = std::make_shared<YenToEuroConverter>();
-  double aLotOfYen = 10000;
-  aLotOfEuros = yentoeuro->convert(aLotOfYen);
-  std::cout << yentoeuro->toString() << " has converted "<< aLotOfYen << " Yen to " << aLotOfEuros <<" Euros!\n"<<std::endl;
-
-  //Gold to Euro
-
-  auto goldtoeuro = std::make_shared<GoldToEuroConverter>();
-  double gold = 52;
-  double euro = goldtoeuro->convert(gold);
-  std::cout << goldtoeuro->toString() << " has converted " << gold << "oz to " << euro << " Euros!\n"<<std::endl;
- 
+  else if(arg1 == "GoldToEuro")
+  {
+    //Gold to Euro
+    auto goldtoeuro = std::make_shared<GoldToEuroConverter>();
+    double euro = goldtoeuro->convert(arg2);
+    std::cout << goldtoeuro->toString() << " has converted " << arg2 << "oz to " << euro << " Euros!\n"<<std::endl;
+  }
+  
+  
+  else if(arg1 == "FahrenheitToCelsius")
+  {
   //Fahrenheit to Celsius Converter
+    auto ftoc = std::make_shared<FahrenheitToCelsiusConverter>();
+    double celsius = ftoc->convert(arg2);
+    std::cout << ftoc->toString() << " has converted "<< arg2 << " Fahrenheit to " << celsius <<" Celsius!\n"<<std::endl;  
+  }
 
-  auto ftoc = std::make_shared<FahrenheitToCelsiusConverter>();
-  double fahrenheit = 80;
-  double celsius = ftoc->convert(fahrenheit);
-  std::cout << ftoc->toString() << " has converted "<< fahrenheit << " Fahrenheit to " << celsius <<" Celsius!\n"<<std::endl;  
-
-  //Celsius to Fahrenheit Converter
-
-  auto ctof = std::make_shared<CelsiusToFahrenheitConverter>();
-  celsius = 100;
-  fahrenheit = ctof->convert(celsius);
-  std::cout << ctof->toString() << " has converted "<< fahrenheit << " Celsius to " << celsius <<" Fahrenheit!\n"<<std::endl;    
+  else if(arg1 == "CelsiusToFahrenheit")
+  {
+    //Celsius to Fahrenheit Converter
+    auto ctof = std::make_shared<CelsiusToFahrenheitConverter>();
+    double fahrenheit = ctof->convert(arg2);
+    std::cout << ctof->toString() << " has converted "<< arg2 << " Celsius to " << fahrenheit <<" Fahrenheit!\n"<<std::endl;    
+  }
+  else
+  {
+    std::cout << "ERROR" <<std::endl;
+  }
 
 
   return 0;
